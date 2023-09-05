31c9802de3ea12361aa845cf94fe47f1221a080c
TreNgheDiCode <gabayan170@gmail.com>
2023-09-05 21:49:35 +0700

    State

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import CardList from './components/card-list/CardList';
+import Search from './components/search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
